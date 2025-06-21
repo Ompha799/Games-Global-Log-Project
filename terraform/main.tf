3e3f1cb42014d5978b32f2c08b5a645cec582c3a
ompha mudau <mudauompha87@gmail.com>
2025-06-21 19:47:09 +0200

    add pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "aws_api_gateway_integration" "get_log_lambda" {
   rest_api_id             = aws_api_gateway_rest_api.log_api.id
   resource_id             = aws_api_gateway_resource.log_resource.id
   http_method             = aws_api_gateway_method.get_log.http_method
-  integration_http_method = "POST"  
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.get_logs_function.invoke_arn
 }
