1658a6d0bec100c4800823b2c3fbaf570bbc8c3f
ompha mudau <mudauompha87@gmail.com>
2025-06-22 18:10:04 +0200

    add security checks to tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,28 +2,28 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = var.tf_state_bucket_name
+# resource "aws_s3_bucket" "tf_state_bucket" {
+#   bucket = var.tf_state_bucket_name
   
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_dynamodb_table" "tf_lock_table" {
-  name         = var.tf_lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# resource "aws_dynamodb_table" "tf_lock_table" {
+#   name         = var.tf_lock_table_name
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
   
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
 
 terraform {
